c9db4670e52f76d4e2811da7e032f93cdac3d095
TrinhDangDang <73269352+TrinhDangDang@users.noreply.github.com>
2024-08-23 20:39:40 -0500

    added basic food

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ const ctx = canvas.getContext("2d");
 
 
 class Pacman {
-  constructor(x, y, width, height, direction) {
+  constructor(x, y, width, height) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
-    this.direction = direction;
+    this.direction = "";
+    this.nextDirection = "";
    
   }
   draw(){
@@ -31,7 +32,22 @@ class Pacman {
       this.y + this.height >= canvas.height
     );
   }
- 
+  
+  changeDirectionIfPossible(){
+      if (this.direction == this.nextDirection) return;
+        let tempDirection = this.direction;
+        this.direction = this.nextDirection;
+        this.moveForward();
+        if (this.hitwall()) {
+            this.pauseMoving();
+            this.direction = tempDirection;
+        } else {
+            this.pauseMoving();
+        }
+    }
+    
+
+  
   pauseMoving(){
     switch(this.direction){
       case "ArrowUp":
@@ -70,26 +86,33 @@ class Pacman {
 window.addEventListener('keydown', (event) => {
   switch (event.key) {
     case 'ArrowUp':
-      pacman.direction = 'ArrowUp';
+      pacman.nextDirection = 'ArrowUp';
       break;
     case 'ArrowDown':
-      pacman.direction = 'ArrowDown';
+      pacman.nextDirection = 'ArrowDown';
       break;
     case 'ArrowLeft':
-      pacman.direction = 'ArrowLeft';
+      pacman.nextDirection = 'ArrowLeft';
       break;
     case 'ArrowRight':
-      pacman.direction = 'ArrowRight';
+      pacman.nextDirection = 'ArrowRight';
       break;
   }
 });
+
+
+
 const pacman = new Pacman(5,5, 4, 4, '')
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+  drawMap();
+  drawFood();
+  pacman.changeDirectionIfPossible();
   pacman.moveForward();
   let gridX = Math.floor(pacman.x/blockSize);
   let gridY = Math.floor(pacman.y/blockSize);
-  drawMap();
+  
+  
     document.getElementById('output').textContent = `${pacman.x}   ${pacman.y}  ${gridX} ${gridY}`
   if (pacman.hitwall()){
     pacman.pauseMoving();
@@ -128,6 +151,17 @@ let map = [
 
 const blockSize = 5;
 
+
+function drawFood() {
+  map.forEach(( row, y) => {
+    row.forEach(( cell , x) => {
+      if (cell == 2) {
+         // Set a color for the food
+        ctx.fillRect(x * blockSize, y * blockSize, 2, 2);
+      }
+    });
+  });
+}
 function drawMap(){
 map.forEach((row, y)=> {
   row.forEach((column, x)=> {
@@ -138,7 +172,6 @@ map.forEach((row, y)=> {
 })
 }
 
-drawMap();
 
 let gameInternal = setInterval(animate, 1000/60);
 
